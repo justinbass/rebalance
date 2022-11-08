e1d95b4b9d755216dc413fd076785d790ca76760
Justin Bass <justinalanbass@gmail.com>
2022-11-08 19:36:30 +0000

    Flexible fund specification

--- rebalance_portfolio.py
+++ rebalance_portfolio.py
@@ -2,8 +2,8 @@
 import argparse
 import sys
 
-# Default, e.g. formats correctly up to $100,000.00
-RIGHT_JUST_MAX = 11
+# Default, e.g. formats correctly up to $9,999,999.99
+RIGHT_JUST_MAX = 14
 
 # In format_dollar, add 5 spaces for padding dollar sign, negative, and cents: "$", "-" ".56"
 DOLLAR_PAD = 5
@@ -18,19 +18,19 @@ def format_dollar(n, rj=RIGHT_JUST_MAX):
         return "-${0:,.2f}".format(-n).rjust(rj)
 
 
-def format_perc(n):
+def format_percentage(n):
     return "{0:.2f}%".format(100 * n).rjust(RIGHT_JUST_MAX)
 
 
-def optimal_add(add_amount, fund_num, add_funds, owned, percs):
+def optimal_add(add_amount, fund_num, add_funds, owned, percentages):
     """
     Optimal amounts to add to fund_num to make deviations from ideal proportion equal
     """
     sum_amount = sum([owned[i] for i in add_funds])
-    sum_percentages = sum([percs[i] for i in add_funds])
+    sum_percentages = sum([percentages[i] for i in add_funds])
 
     # Expression derived by hand
-    return percs[fund_num] * (add_amount + sum_amount) / sum_percentages - owned[fund_num]
+    return percentages[fund_num] * (add_amount + sum_amount) / sum_percentages - owned[fund_num]
 
 
 def optimal_add_others(fund_num, add_funds, owned, percentages):
@@ -45,17 +45,11 @@ def optimal_add_others(fund_num, add_funds, owned, percentages):
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Rebalance a Vanguard three or four-fund portfolio.')
-    parser.add_argument('add', type=float, help="Amount to add to portfolio")
-    parser.add_argument('vtsax', type=float, help="VTSAX amount owned")
-    parser.add_argument('vtiax', type=float, help="VTIAX amount owned")
-    parser.add_argument('vbtlx', type=float, help="VBTLX amount owned")
-    parser.add_argument('vtibx', type=float, help="VTIBX amount owned")
-    parser.add_argument('--vtsax-desired', type=float, default=0.535, help="VTSAX amount desired")
-    parser.add_argument('--vtiax-desired', type=float, default=0.362, help="VTIAX amount desired")
-    parser.add_argument('--vbtlx-desired', type=float, default=0.071, help="VBTLX amount desired")
-    parser.add_argument('--vtibx-desired', type=float, default=0.032, help="VTIBX amount desired")
-    parser.add_argument('--no-vtibx', action='store_true', help="exclude VTIBX from consideration")
+    parser = argparse.ArgumentParser(description='Rebalance a portfolio.')
+    parser.add_argument('--add', type=float, help="Amount to add to portfolio", required=True)
+    parser.add_argument('--owned', nargs='+', type=float, help="Funds owned", required=True)
+    parser.add_argument('--desired', nargs='+', type=float, help="Fund ratio desired", required=True)
+    parser.add_argument('--names', nargs='+', type=str, help="Fund ticker names", required=True)
 
     return parser.parse_args()
 
@@ -65,31 +59,25 @@ def main():
 
     args = parse_args()
 
-    added = args.add
+    add = args.add
+    owned = args.owned
+    desired = args.desired
+    names = args.names
 
-    if args.no_vtibx:
-        percentages = [args.vtsax_desired, args.vtiax_desired, args.vbtlx_desired + args.vtibx_desired]
-        owned = [args.vtsax, args.vtiax, args.vbtlx]
-        funds = ["VTSAX", "VTIAX", "VBTLX"]
-    else:
-        percentages = [args.vtsax_desired, args.vtiax_desired, args.vbtlx_desired, args.vtibx_desired]
-        owned = [args.vtsax, args.vtiax, args.vbtlx, args.vtibx]
-        funds = ["VTSAX", "VTIAX", "VBTLX", "VTIBX"]
-
-    funds_str = "".join(map(lambda x: x.rjust(RIGHT_JUST_MAX), funds))
-    percentages_str = "".join(map(lambda x: format_perc(x), percentages))
+    funds_str = "".join(map(lambda x: x.rjust(RIGHT_JUST_MAX), names))
+    percentages_str = "".join(map(lambda x: format_percentage(x), desired))
 
     print("Funds:        " + funds_str)
     print("Ideal ratios: " + percentages_str)
 
     current_total = sum(owned)
 
-    most_decimals = len(str(int((max([added, current_total])))))
+    most_decimals = len(str(int((max([add, current_total])))))
     RIGHT_JUST_MAX = most_decimals + DOLLAR_PAD
 
     print()
 
-    if added < - current_total:
+    if add < - current_total:
         print("You do not have enough money to withdraw the desired amount")
         sys.exit(1)
 
@@ -103,22 +91,21 @@ def main():
     minimum_no_sell_add_amount = 0
     minimum_no_buy_remove_amount = 0
     for fund_num, fund_owned in enumerate(owned):
-        opt_oth = optimal_add_others(fund_num, all_funds, owned, percentages)
+        opt_oth = optimal_add_others(fund_num, all_funds, owned, desired)
         minimum_no_sell_add_amount = max(minimum_no_sell_add_amount, opt_oth)
         minimum_no_buy_remove_amount = min(minimum_no_buy_remove_amount, opt_oth)
 
         ratio = fund_owned / current_total if current_total else 0
 
-        deviation = ratio / percentages[fund_num]
-
-        print("Fund", funds[fund_num],
+        deviation = ratio / desired[fund_num]
+        print("Fund", names[fund_num],
               "owned:", format_dollar(owned[fund_num]),
-              "  Ratio:", format_perc(ratio),
-              "  Deviation:", format_perc(deviation))
+              "  Ratio:", format_percentage(ratio),
+              "  Deviation:", format_percentage(deviation))
 
     print("Total:" + format_dollar(current_total))
     print()
-    print("Adding:", format_dollar(added))
+    print("Adding:", format_dollar(add))
     print("Minimum needed to add to rebalance perfectly without selling:", format_dollar(minimum_no_sell_add_amount))
     print("Minimum needed to sell to rebalance perfectly without buying:", format_dollar(minimum_no_buy_remove_amount))
     print()
@@ -127,15 +114,15 @@ def main():
 
     optimal_change = list()
     for fund_num, fund_owned in enumerate(owned):
-        opt_this = optimal_add(added, fund_num, all_funds, owned, percentages)
-        optimal_change.append(opt_this if opt_this < 0 and added < 0 or opt_this > 0 and added > 0 else 0)
+        opt_this = optimal_add(add, fund_num, all_funds, owned, desired)
+        optimal_change.append(opt_this if opt_this < 0 and add < 0 or opt_this > 0 and add > 0 else 0)
 
-        print("Fund", funds[fund_num],
+        print("Fund", names[fund_num],
               "owned:", format_dollar(owned[fund_num]),
               "  Add or sell:", format_dollar(opt_this))
 
-    if added != 0:
-        suboptimal = 0 > added > minimum_no_buy_remove_amount or 0 < added < minimum_no_sell_add_amount
+    if add != 0:
+        suboptimal = 0 > add > minimum_no_buy_remove_amount or 0 < add < minimum_no_sell_add_amount
         suboptimal_string = "suboptimal" if suboptimal else "perfect"
 
         print()
@@ -144,7 +131,7 @@ def main():
 
         add_to_each = list()
         for v in optimal_change:
-            add_to_each.append(added * v / sum(optimal_change))
+            add_to_each.append(add * v / sum(optimal_change))
 
         owned_plus_added = list()
         for i, v in enumerate(owned):
@@ -153,15 +140,15 @@ def main():
         suboptimal = False
 
         for fund_num, fund_owned in enumerate(owned_plus_added):
-            ratio = fund_owned / (current_total + added) if current_total + added else 0
+            ratio = fund_owned / (current_total + add) if current_total + add else 0
 
-            deviation = ratio / percentages[fund_num]
+            deviation = ratio / desired[fund_num]
 
-            print("Fund", funds[fund_num],
+            print("Fund", names[fund_num],
                   "owned:", format_dollar(fund_owned),
                   "  To add:", format_dollar(add_to_each[fund_num]),
-                  "  Ratio:", format_perc(ratio),
-                  "  Deviation:", format_perc(deviation))
+                  "  Ratio:", format_percentage(ratio),
+                  "  Deviation:", format_percentage(deviation))
 
         if suboptimal:
             print()
